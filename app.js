df7eb8ce199c2cded3228135c010e9ee075424af
brett burrington <bburrington13@gmail.com>
2023-08-04 12:54:00 -0700

    add role

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ const { connectToDB, queryDatabase, closeConnection } = require('./db');
 // Function to view all departments
 async function viewDepartments() {
   try {
-    // Fetch all departments from the database
     const departments = await queryDatabase('SELECT * FROM department');
-
-    // Display departments in a tabular format using console.table
     console.table(departments);
   } catch (error) {
-    // If an error occurs during the database query, handle and display the error message
     console.error('Error:', error.message);
   }
 }
@@ -18,13 +14,9 @@ async function viewDepartments() {
 // Function to view all roles
 async function viewRoles() {
   try {
-    // Fetch all roles from the database
     const roles = await queryDatabase('SELECT * FROM role');
-
-    // Display roles in a tabular format using console.table
     console.table(roles);
   } catch (error) {
-    // If an error occurs during the database query, handle and display the error message
     console.error('Error:', error.message);
   }
 }
@@ -32,79 +24,98 @@ async function viewRoles() {
 // Function to view all employees
 async function viewEmployees() {
   try {
-    // Fetch all employees from the database
     const employees = await queryDatabase('SELECT * FROM employee');
-
-    // Display employees in a tabular format using console.table
     console.table(employees);
   } catch (error) {
-    // If an error occurs during the database query, handle and display the error message
     console.error('Error:', error.message);
   }
 }
 
+// Function to add a new role
+async function addRole() {
+  try {
+    // Prompt the user to enter role details
+    const roleDetails = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the role title:',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the role salary:',
+      },
+      {
+        type: 'input',
+        name: 'department_id',
+        message: 'Enter the department ID for the role:',
+      },
+    ]);
 
+    // Insert the new role into the database
+    await queryDatabase(
+      'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
+      [roleDetails.title, roleDetails.salary, roleDetails.department_id]
+    );
+
+    console.log('New role added successfully!');
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
 
 // Function to display the main menu and handle user actions
 async function showMainMenu() {
   try {
-    // Prompt the user with a list of choices using inquirer
     const answer = await inquirer.prompt([
       {
         type: 'list',
         name: 'action',
         message: 'What would you like to do?',
         choices: [
-          // List of available actions for the user
           'View all departments',
           'View all roles',
           'View all employees',
+          'Add a role', // Added the "Add a role" option to the main menu
           'Exit',
         ],
       },
     ]);
 
-    // Perform the corresponding action based on the user's choice
     switch (answer.action) {
       case 'View all departments':
-        // Call the function to view all departments
         await viewDepartments();
         break;
       case 'View all roles':
-        // Call the function to view all roles
         await viewRoles();
         break;
       case 'View all employees':
-        // Call the function to view all employees
         await viewEmployees();
         break;
+      case 'Add a role': // When user selects "Add a role", call the addRole() function
+        await addRole();
+        break;
       case 'Exit':
-        // Exit the application and close the database connection
         console.log('Goodbye!');
         closeConnection();
         break;
       default:
-        // If the user enters an invalid choice, display an error message and show the main menu again
         console.log('Invalid choice. Please select a valid option.');
         await showMainMenu();
     }
   } catch (error) {
-    // If any unexpected error occurs during the main menu interaction, handle and display the error message
     console.error('Error:', error.message);
-    closeConnection(); // Close the database connection when an error occurs
+    closeConnection();
   }
 }
 
-// Main function to start the application
 async function main() {
   console.log('Working...');
-  await connectToDB(); // Connect to the database
+  await connectToDB();
 
   console.log('Welcome to Employee Tracker ');
-  await showMainMenu(); // Show the main menu and handle user actions
+  await showMainMenu();
 }
 
-// Call the main function to start the application
 main();
-
-// Export any necessary functions (list of exported functions remains unchanged)
--- seed.sql
+++ seed.sql
@@ -9,21 +9,21 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Software Engineer', 80000.00, 1),
-  ('Sales Manager', 100000.00, 2),
+  ('Software Engineer', 82500.00, 1),
+  ('Sales Manager', 103000.00, 2),
   ('Marketing Specialist', 60000.00, 3),
-  ('HR Administrator', 50000.00, 4);
+  ('HR Administrator', 51450.00, 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
-  ('John', 'Doe', 1),
-  ('Jane', 'Smith', 2),
-  ('Michael', 'Johnson', 3),
-  ('Mary', 'Williams', 4);
+  ('Mike', 'Uune', 1),
+  ('Alex', 'Koster', 2),
+  ('Michael', 'Reftin', 3),
+  ('Andre', 'Willison', 4);
 
 
 UPDATE employee AS e
-JOIN employee AS manager ON manager.first_name = 'John' AND manager.last_name = 'Doe'
+JOIN employee AS manager ON manager.first_name = 'Mike' AND manager.last_name = 'Uune'
 SET e.manager_id = manager.id
-WHERE e.first_name IN ('Jane', 'Michael');
+WHERE e.first_name IN ('Mike', 'Alex', 'Michael', 'Andre');
